b997e19d0c93b3eb9fd6bd37859af2446a6a0584
KNTXTremE <kan.horwang@gmail.com>
2020-05-29 20:16:06 +0700

    Add daily cases graph

--- main.py
+++ main.py
@@ -31,115 +31,125 @@ colors = {
 }
 
 app.layout = html.Div(style={'backgroundColor': colors['background'], 'height': '100vh'}, children=[
-    html.H1(
-        children='COVID19 Dashboard',
-        style={
-            'textAlign': 'center',
-            'color': colors['text'],
-            'padding': '5px',
-            'font-weight': 'bold'
-        }
-    ),
-
-    html.Div(children='For educational purpose only', style={
-        'textAlign': 'center',
-        'color': colors['text']
-    }),
-
     html.Div(style={'backgroundColor': colors['background'], 'justify-content': 'center', 'display': 'flex'}, children=[
-        html.Div(style={'backgroundColor': colors['background'], 'width': '25%', 'margin': '20px'}, children=[
-            html.H6(
-                    children='Select country',
-                    style={
-                        'textAlign': 'center',
-                        'color': colors['text']
-                    }
-                ),
-
-            dcc.Dropdown(
-                    id="country",
-                    options=[{
-                        'label': i,
-                        'value': i
-                    } for i in country_options],
-                    value='All countries')
-        ])
-    ]),
-
-    
-
-    html.Div(style={'backgroundColor': colors['background'], 'margin': '10px', 'justify-content': 'center', 'display': 'flex'}, children=[
         html.Div(style={'backgroundColor': colors['background'], 'margin': '20px', 'display': 'inline-block'}, children=[
-            html.H4(
-                children='TOTAL CONFIRMED',
+            html.H1(
+                children='COVID19 Dashboard',
                 style={
                     'textAlign': 'center',
-                    'color': colors['text']
+                    'color': colors['text'],
+                    'padding': '5px',
+                    'font-weight': 'bold'
                 }
             ),
 
-            html.H1(
-                id='total_confirmed',
-                style={
-                    'textAlign': 'center',
-                    'color': colors['red'],
-                    'font-weight': 'bold'
-                }
-            )
+            html.Div(children='For educational purpose only', style={
+                'textAlign': 'center',
+                'color': colors['text']
+            }),
+
+            html.Div(style={'backgroundColor': colors['background'], 'justify-content': 'center', 'display': 'flex'}, children=[
+                html.Div(style={'backgroundColor': colors['background'], 'width': '50%', 'margin': '20px'}, children=[
+                    html.H6(
+                            children='Select country',
+                            style={
+                                'textAlign': 'center',
+                                'color': colors['text']
+                            }
+                        ),
+
+                    dcc.Dropdown(
+                            id="country",
+                            options=[{
+                                'label': i,
+                                'value': i
+                            } for i in country_options],
+                            value='All countries')
+                ])
+            ]),
+
+            
+
+            html.Div(style={'backgroundColor': colors['background'], 'margin': '10px', 'justify-content': 'center', 'display': 'flex'}, children=[
+                html.Div(style={'backgroundColor': colors['background'], 'margin': '20px', 'display': 'inline-block'}, children=[
+                    html.H4(
+                        children='TOTAL CONFIRMED',
+                        style={
+                            'textAlign': 'center',
+                            'color': colors['text']
+                        }
+                    ),
+
+                    html.H1(
+                        id='total_confirmed',
+                        style={
+                            'textAlign': 'center',
+                            'color': colors['red'],
+                            'font-weight': 'bold'
+                        }
+                    )
+                ]),
+                
+
+                html.Div(style={'backgroundColor': colors['background'], 'margin': '20px', 'display': 'inline-block'}, children=[
+                    html.H4(
+                        children='TOTAL DEATHS',
+                        style={
+                            'textAlign': 'center',
+                            'color': colors['text']
+                        }
+                    ),
+
+                    html.H1(
+                        id='total_deaths',
+                        style={
+                            'textAlign': 'center',
+                            'color': colors['white'],
+                            'font-weight': 'bold'
+                        }
+                    )
+                ]),
+                
+                html.Div(style={'backgroundColor': colors['background'], 'margin': '20px', 'display': 'inline-block'}, children=[
+                    html.H4(
+                        children='TOTAL RECOVERED',
+                        style={
+                            'textAlign': 'center',
+                            'color': colors['text']
+                        }
+                    ),
+
+                    html.H1(
+                        id='total_recovered',
+                        style={
+                            'textAlign': 'center',
+                            'color': colors['green'],
+                            'font-weight': 'bold'
+                        }
+                    )
+                ]),
+                
+            ])
         ]),
-        
 
         html.Div(style={'backgroundColor': colors['background'], 'margin': '20px', 'display': 'inline-block'}, children=[
-            html.H4(
-                children='TOTAL DEATHS',
-                style={
-                    'textAlign': 'center',
-                    'color': colors['text']
-                }
-            ),
-
-            html.H1(
-                id='total_deaths',
-                style={
-                    'textAlign': 'center',
-                    'color': colors['white'],
-                    'font-weight': 'bold'
-                }
-            )
-        ]),
-        
-        html.Div(style={'backgroundColor': colors['background'], 'margin': '20px', 'display': 'inline-block'}, children=[
-            html.H4(
-                children='TOTAL RECOVERED',
-                style={
-                    'textAlign': 'center',
-                    'color': colors['text']
-                }
-            ),
-
-            html.H1(
-                id='total_recovered',
-                style={
-                    'textAlign': 'center',
-                    'color': colors['green'],
-                    'font-weight': 'bold'
-                }
-            )
-        ]),
-        
-    ]),
-
-    html.Div(style={'backgroundColor': colors['background'], 'justify-content': 'center', 'display': 'flex'}, children=[
-
-        html.Div(style={'backgroundColor': colors['background'], 'margin': '10px', 'display': 'inline-block'}, children=[
             dcc.Graph(
                 id='total-confirmed-map'
             )
-        ]),
+        ])
+    ]),
+
+    html.Div(style={'backgroundColor': colors['background'], 'justify-content': 'center', 'display': 'flex'}, children=[
         
         html.Div(style={'backgroundColor': colors['background'], 'margin': '10px', 'display': 'inline-block'}, children=[
             dcc.Graph(
                 id='total-confirmed-graph'
+            )
+        ]),
+
+        html.Div(style={'backgroundColor': colors['background'], 'margin': '10px', 'display': 'inline-block'}, children=[
+            dcc.Graph(
+                id='total-confirmed-daily'
             )
         ])
     ])
@@ -150,6 +160,7 @@ app.layout = html.Div(style={'backgroundColor': colors['background'], 'height': 
 @app.callback(
     [dash.dependencies.Output('total-confirmed-map', 'figure'),
     dash.dependencies.Output('total-confirmed-graph', 'figure'),
+    dash.dependencies.Output('total-confirmed-daily', 'figure'),
     dash.dependencies.Output('total_confirmed', 'children'),
     dash.dependencies.Output('total_deaths', 'children'),
     dash.dependencies.Output('total_recovered', 'children')],
@@ -164,7 +175,7 @@ def update_graph(country):
         df_plot = df[df['Country_Region'] == country][['Confirmed', 'Deaths', 'Recovered']].groupby('date').sum()
         loc_lat = location[location['Country_Region'] == country].drop_duplicates().reset_index()['Lat'][0]
         loc_lng = location[location['Country_Region'] == country].drop_duplicates().reset_index()['Long_'][0]
-        zoom = 3
+        zoom = 4
 
     total_confirmed = df_plot.tail(1)['Confirmed']
     total_deaths = df_plot.tail(1)['Deaths']
@@ -173,7 +184,10 @@ def update_graph(country):
     trace1 = go.Scatter(x=df_plot.index, y=df_plot['Confirmed'], name='Confirmed')
     trace2 = go.Scatter(x=df_plot.index, y=df_plot['Deaths'], name='Deaths')
     trace3 = go.Scatter(x=df_plot.index, y=df_plot['Recovered'], name='Recovered')
-    fig = go.Figure(data=go.Scattermapbox(
+
+    daily = go.Bar(x=df_plot.index, y=df_plot['Confirmed'] - df_plot['Confirmed'].shift(1).fillna(value=0))
+
+    fig_map = go.Figure(data=go.Scattermapbox(
         lon = today_stats['Long_'],
         lat = today_stats['Lat'],
         text = today_stats['Country_Region'],
@@ -184,11 +198,10 @@ def update_graph(country):
             )
         ),
         layout = go.Layout(
-            title = 'Total confirmed map',
             paper_bgcolor= colors['background'],
             plot_bgcolor= colors['background'],
             font= {
-                        'color': colors['text']
+                    'color': colors['text']
                 },
             mapbox_style="carto-darkmatter",
             mapbox=dict(
@@ -199,10 +212,12 @@ def update_graph(country):
                 ),
                 pitch=0,
                 zoom=zoom
-            )
+            ),
+            margin=dict(t=0, b=0, l=0, r=0)
         )
     )
-    return fig, {
+
+    fig_graph = {
         'data': [trace1, trace2, trace3],
         'layout': {
                 'title': "Total number of infected",
@@ -211,7 +226,19 @@ def update_graph(country):
                 'font': {
                     'color': colors['text']
                 }}
-    }, '{:,}'.format(int(total_confirmed)), '{:,}'.format(int(total_deaths)), '{:,}'.format(int(total_recovered))
+    }
+    fig_daily = {
+        'data': [daily],
+        'layout': {
+                'title': "Number of infected daily",
+                'paper_bgcolor': colors['background'],
+                'plot_bgcolor': colors['background'],
+                'font': {
+                    'color': colors['text']
+                }}
+    }
+
+    return fig_map, fig_graph, fig_daily, '{:,}'.format(int(total_confirmed)), '{:,}'.format(int(total_deaths)), '{:,}'.format(int(total_recovered))
 
 if __name__ == '__main__':
     app.run_server(debug=True)
